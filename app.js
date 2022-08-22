93dbc1f99b4a2dc7f68c262cfafbe97ad80220d9
phryszlo <phryszlo@gmail.com>
2022-08-21 23:22:32 -0400

    i can't go any farther tonight.

--- app.js
+++ app.js
@@ -108,9 +108,10 @@ const displayNextWorkHistoryItem = () => {
 /* BACKGROUND IMAGE ARRAY */
 let idxBg = 0;
 const backgroundImages = [
+  "black.png",
   "silhouetted.png",
   "bmmod1.png",
-  "sunset1.jpg",
+  // "sunset1.jpg",
   "phoenix.jpg",
   // "bmmod2.png",
   "sunset2.jpg"
