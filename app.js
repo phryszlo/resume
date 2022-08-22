f6125174465ce7b85a914974d6ab455bbb53803f
phryszlo <phryszlo@gmail.com>
2022-08-22 07:05:10 -0400

    moved resume into module, click events to DOMContentLoaded

--- app.js
+++ app.js
@@ -1,63 +1,30 @@
+import resume from './resume.js';
 
-// import { workHistory, schoolHistory } from './data.js';
-
-
-
-const workHistory = {
-  jobs: [
-    {
-      company: 'SBM LLC',
-      location: 'Proctor and Gamble Global HQ',
-      title: 'Meeting Services Tech',
-      dates: '2018-2020',
-      description: `Developed applications and scripts for managing event scheduling and equipment inventory; 
-      edited audio/visual control system programs and Christie Spyder video processor configurations for Smale Auditorium; 
-      provided support for meeting room and video conferencing technical services; 
-      provided video production services including control room audio, camera operation, live switching, routing, setup and teardown.`
-    },
-    {
-      company: 'University of Cincinnati',
-      location: "Cincinnati, OH, UC Main Campus",
-      title: "Computer and Information Analyst",
-      dates: "2005-2015",
-      description: `Developed custom software for inventory, trouble ticketing, scheduling, equipment delivery, administrative processes. 
-      Administered multiple domains, database, course streaming, and other servers; 
-      developed innovative applications and created technical solutions where existing systems did not meet customer needs; 
-      maintained and deployed system images. Created and maintained incident management, inventory, and other web and administrative software; 
-      collaborated with staff and faculty to solve classroom hardware and software needs. 
-      Performed general computer support and system imaging. `
-    },
-    {
-      company: "University of Cincinnati",
-      location: "Cincinnati, OH, UC Main Campus",
-      title: "ResNet Manager",
-      description: "Supervised the installation of network connectivity in University dormitories.",
-      dates: "2000-2001",
-      description: `Managed the ResNet program, directing a team of 15 student workers to set up computer networking for student housing and residence halls.`
-    }
-  ]
-}
-const schoolHistory = {
-  schools: [
-    {
-      school: 'The George Washington University',
-      years: '1991-1994',
-      areaOfStudy: 'Liberal Arts/Music'
-    },
-    {
-      school: 'University of Cincinnati College Conservatory of Music',
-      years: '1996-1999',
-      areaOfStudy: 'Music Theory and Composition'
-    },
-    {
-      school: 'University of Cincinnati College of Applied Science',
-      years: '2002-2005',
-      areaOfStudy: 'Information Engineering Technology'
-    }
-  ]
-}
+const summary = resume.summary;
+const workHistory = resume.workHistory;
+const schoolHistory = resume.schoolHistory;
 
 
+/* DOMCONTENTLOADED 
+after all elements have been rendered 
+    check which image has been set as the background
+    and find the index of its match in the bg array */
+    window.addEventListener('DOMContentLoaded', () => {
+      const target = document.querySelector(".background");
+      const nowShowing = target.src.substring(target.src.indexOf('/') + 1);
+      console.log(nowShowing.substring(nowShowing.lastIndexOf('/') + 1));
+      const idxNowShowing = backgroundImages.findIndex((path) =>
+        path === nowShowing.substring(nowShowing.lastIndexOf('/') + 1)
+      );
+      idxBg = idxNowShowing;
+
+
+      // and then add the event listeners
+      document.querySelector(".arrow.school").addEventListener("click", displayNextSchoolHistoryItem);
+      document.querySelector(".arrow.work").addEventListener("click", displayNextWorkHistoryItem);
+      document.querySelector(".change-color").addEventListener("click", cycleBackgroundImages);
+
+    });
 
 /*
   WORK AND SCHOOL HISTORY COUNTERS
@@ -118,19 +85,7 @@ const backgroundImages = [
 
 ];
 
-/* DOMCONTENTLOADED 
-after all elements have been rendered 
-    check which image has been set as the background
-    and find the index of its match in the bg array */
-window.addEventListener('DOMContentLoaded', () => {
-  const target = document.querySelector(".background");
-  const nowShowing = target.src.substring(target.src.indexOf('/') + 1);
-  console.log(nowShowing.substring(nowShowing.lastIndexOf('/') + 1));
-  const idxNowShowing = backgroundImages.findIndex((path) =>
-    path === nowShowing.substring(nowShowing.lastIndexOf('/') + 1)
-  );
-  idxBg = idxNowShowing;
-});
+
 
 const cycleBackgroundImages = () => {
   if (idxBg === (backgroundImages.length - 1)) {
---
+++ resume.js
@@ -0,0 +1,62 @@
+
+
+const resume = {
+  summary: {
+
+  },
+  workHistory: {
+    jobs: [
+      {
+        company: 'SBM LLC',
+        location: 'Proctor and Gamble Global HQ',
+        title: 'Meeting Services Tech',
+        dates: '2018-2020',
+        description: `Developed applications and scripts for managing event scheduling and equipment inventory; 
+      edited audio/visual control system programs and Christie Spyder video processor configurations for Smale Auditorium; 
+      provided support for meeting room and video conferencing technical services; 
+      provided video production services including control room audio, camera operation, live switching, routing, setup and teardown.`
+      },
+      {
+        company: 'University of Cincinnati',
+        location: "Cincinnati, OH, UC Main Campus",
+        title: "Computer and Information Analyst",
+        dates: "2005-2015",
+        description: `Developed custom software for inventory, trouble ticketing, scheduling, equipment delivery, administrative processes. 
+      Administered multiple domains, database, course streaming, and other servers; 
+      developed innovative applications and created technical solutions where existing systems did not meet customer needs; 
+      maintained and deployed system images. Created and maintained incident management, inventory, and other web and administrative software; 
+      collaborated with staff and faculty to solve classroom hardware and software needs. 
+      Performed general computer support and system imaging. `
+      },
+      {
+        company: "University of Cincinnati",
+        location: "Cincinnati, OH, UC Main Campus",
+        title: "ResNet Manager",
+        description: "Supervised the installation of network connectivity in University dormitories.",
+        dates: "2000-2001",
+        description: `Managed the ResNet program, directing a team of 15 student workers to set up computer networking for student housing and residence halls.`
+      }
+    ]
+  },
+  schoolHistory: {
+    schools: [
+      {
+        school: 'The George Washington University',
+        years: '1991-1994',
+        areaOfStudy: 'Liberal Arts/Music'
+      },
+      {
+        school: 'University of Cincinnati College Conservatory of Music',
+        years: '1996-1999',
+        areaOfStudy: 'Music Theory and Composition'
+      },
+      {
+        school: 'University of Cincinnati College of Applied Science',
+        years: '2002-2005',
+        areaOfStudy: 'Information Engineering Technology'
+      }
+    ]
+  }
+}
+
+export default resume;
